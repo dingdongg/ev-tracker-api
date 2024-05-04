e90a27f3376d4b14df4429ffe0771e2fe81a322a
dingdongg <cs.dg.kim@gmail.com>
2024-05-03 23:39:48 -0600

    accept file submissions from frontend

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module ev-tracker/src
 go 1.21.6
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
--- src.go
+++ src.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"bytes"
 	"ev-tracker/src/handlers"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 func main() {
@@ -11,5 +13,21 @@ func main() {
 
 	http.HandleFunc("/get-pokemon", handlers.FetchPokemonHandler)
 	http.HandleFunc("/update-pokemon", handlers.UpdatePokemonHandler)
+	http.HandleFunc("/post-savefile", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseMultipartForm(1 << 19)
+		var buf bytes.Buffer
+
+		file, header, err := r.FormFile("savefile")
+		if err != nil {
+			fmt.Println("BRUH ", err)
+			return
+		}
+		defer file.Close()
+		
+		fmt.Printf("filename: %s\n", header.Filename)
+		io.Copy(&buf, file)
+
+		fmt.Printf("% x\n", buf)
+	})
 	http.ListenAndServe(":8080", nil)
 }
