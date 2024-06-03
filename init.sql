cc025e4171cd883eae9c8d376ce6f14764c6745a
dingdongg <cs.dg.kim@gmail.com>
2024-06-02 21:24:45 -0600

    change table structure

--- init.sql
+++ init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE pokemons (
-    id          uuid default gen_random_uuid(),
-    name        varchar(50),
-    hp          int,
-    attack      int,
-    defense     int,
-    sp_attack   int,
-    sp_defense  int,
-    speed       int,
+CREATE TABLE IF NOT EXISTS pokemons (
+    id                  smallserial,
+    name                varchar(50),
+    base_hp             smallint,
+    base_attack         smallint,
+    base_defense        smallint,
+    base_sp_attack      smallint,
+    base_sp_defense     smallint,
+    base_speed          smallint,
+    ev_yield            smallint,
+    abilities           TEXT[],
+    types               TEXT[],
     PRIMARY KEY (id)
 );
-
--- insert example value into the above table
-INSERT INTO pokemons VALUES (gen_random_uuid(), 'gengar', 0, 0, 0, 252, 6, 252);
