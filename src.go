65cc1716f724d1843b8b38627d32750bd08ec721
dingdongg <cs.dg.kim@gmail.com>
2024-06-05 14:47:01 -0600

    write mock handler for second endpoint

--- src.go
+++ src.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"ev-tracker/src/handlers"
 	"net/http"
-
+	"ev-tracker/src/handlers"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.ReadSaveFileHandler)
+	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"message": "/SAVE ENDPOINT"}`))
+	})
 	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
 	// http.ListenAndServe(":8080", nil)
 }
