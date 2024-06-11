c9731561c8f4eb8887e39129bf588786716cc047
dingdongg <cs.dg.kim@gmail.com>
2024-06-10 19:56:11 -0600

    create handler to process savefile updates + downloads

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"bytes"
+	"errors"
 	"ev-tracker/src/dao/factory"
+	"os"
 
 	"encoding/json"
 	"fmt"
@@ -12,6 +14,7 @@ import (
 	"strconv"
 
 	ROMparser "github.com/dingdongg/pkmn-rom-parser/v4"
+	rq "github.com/dingdongg/pkmn-rom-parser/v4/rom_writer/req"
 )
 
 type StatResponse struct {
@@ -142,4 +145,101 @@ func ReadSaveFileHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(res)
+}
+
+func writeStat(writeRequest *rq.WriteRequest, stat StatResponse, specifier string) error {
+	var values = [6]uint{stat.Hp, stat.Atk, stat.Def, stat.Spe, stat.Spa, stat.Spd}
+
+	switch specifier {
+	case "BATTLE_STAT":
+		writeRequest.WriteBattleStats(stat.Hp, stat.Atk, stat.Def, stat.Spa, stat.Spd, stat.Spe)
+	case "EV": {
+		packedEV := uint(0)
+		for i, s := range values {
+			val := s << (i * 8)
+			packedEV |= val
+		}
+		writeRequest.WriteEV(packedEV)
+	}
+	case "IV": {
+		packedIV := uint(0)
+		for i, s := range values {
+			val := s << (i * 8)
+			packedIV |= val
+		}
+		writeRequest.WriteIV(packedIV)
+	}
+	default:
+		return errors.New("writeStat() - invalid specifier")
+	}
+
+	return nil
+}
+
+func UpdateSaveFileHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:5173")
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"message":"ERROR: invalid request"}`))
+		return
+	}
+	var yuh []PokemonResponse
+
+	var req []byte
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&yuh)
+
+	fmt.Printf("req: %v\n", req)
+	fmt.Printf("UNMARSHAL'D: %v\n", yuh)
+
+	writeReqs := make([]rq.WriteRequest, 0)
+	
+	for i, p := range yuh {
+		fmt.Println(p)
+
+		writeRequest := rq.NewWriteRequest(uint(i))
+
+		writeStat(&writeRequest, p.BattleStats, "BATTLE_STAT")
+		writeStat(&writeRequest, p.EffortValues, "EV")
+		writeStat(&writeRequest, p.IndivValues, "IV")
+		writeRequest.WriteLevel(p.Level)
+		writeRequest.WriteNickname(p.Name)
+
+		// TODO: change pkmn-rom-parser interface for writing abilities and held items via their names
+
+		writeReqs = append(writeReqs, writeRequest)
+	}
+
+	// fetch the cached savefile from s3 bucket or sth; hardcoded for now
+	savefile, err := os.ReadFile("../pkmn-rom-parser/savefiles/new.sav")
+	if err != nil {
+		w.Write([]byte(`{"message":"failed to open mock savefile"}`))
+		return
+	}
+	
+	res, err := ROMparser.Write(savefile, writeReqs)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Printf("ERROR WHILE UPDATING SAVEFILE: %v\n", err)
+		w.Write([]byte(`{"message":"error hile updatiing savefile"}`))
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/octet-stream")
+	w.Header().Add("Content-Disposition", "attachment; filename=\"savefile\"")
+	w.Header().Add("Content-Length", fmt.Sprint(len(res)))
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message":"cannot open flusher"}`))
+		return
+	}
+
+	fmt.Println("success?")
+
+	w.Write(res)
+	flusher.Flush()
 }
--- src.go
+++ src.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ev-tracker/src/handlers"
 	"ev-tracker/src/mode"
+	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,11 +12,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.ReadSaveFileHandler)
-	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"message": "/SAVE ENDPOINT"}`))
-	})
+	http.HandleFunc("/save", handlers.UpdateSaveFileHandler)
 
 	if mode.DEV_MODE {
+		fmt.Println("Listening on port 8080")
 		http.ListenAndServe(":8080", nil)
 	} else {
 		lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
